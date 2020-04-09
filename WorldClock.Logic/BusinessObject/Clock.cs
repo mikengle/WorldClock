98a1e8637050cd70c386ab87d358d65119cba4df
Michael_Engleder <michael.engleder@o.roteskreuz.at>
2020-04-09 10:32:43 +0200

    create clockcontroller and singleton clock

---
+++ WorldClock.Logic/BusinessObject/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldClock.Logic.BusinessObject
+{
+    internal class Clock
+    {
+        private static Clock instance=null;
+
+        private Clock()
+        {
+            instance = new Clock();
+        }
+
+        public static Clock GetInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ WorldClock.Logic/Contracts/IClockController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldClock.Logic.Contracts
+{
+    public interface IClockController:IDisposable
+    {
+    }
+}
