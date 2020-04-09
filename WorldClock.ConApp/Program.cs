4ed34cd022106e828f013704bfb9e69cf0c6f71e
Michael_Engleder <michael.engleder@o.roteskreuz.at>
2020-04-09 10:22:53 +0200

    Initialize

---
+++ WorldClock.ConApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldClock.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
